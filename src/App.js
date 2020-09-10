7ce775f0d192354b27771eb41c926d02b7fe0cd4
Tolulope Ayemobola <ayemobolatolulope@gmail.com>
2020-09-10 16:53:19 +0100

    Weather app project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import './WeatherApp.js';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <WeatherApp /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/WeatherApp.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class WeatherApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {temp: 0, desc: '', icon: '', loading: true}
+  }
+
+  componentDidMount() {
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&APIkey=YOUR_API_KEY')
+    .then(response => response.json()) 
+    .then(responseData => {
+      this.setState({ 
+         temp: responseData.main.temp,
+        desc: responseData.weather[0].description,
+        icon: responseData.weather[0].icon,
+        loading: false 
+       }); 
+    });
+  }
+  
+  render() {
+    const imgSrc = 'http://openweathermap.org/img/w/' + 
+    this.state.icon + '.png';
+
+    if (this.state.loading) {
+      return <p>Loading</p>;
+    }
+    else {
+      return (
+        <div className="App">
+          <p>Temperature: {this.state.temp} °C</p>
+          <p>Description: {this.state.desc}</p>
+          <img src={imgSrc} alt="Weather icon" />
+        </div>
+      );
+    }
+ }
+}
+
+export default WeatherApp;
+
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+constructor(props) {
+super(props);
+this.state = {temp: 0, desc: '', icon: ''}
+}
+componentDidMount() {
+fetch('https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=bfe5ae53a69a94c7f75699db3f7ffaf7')
+.then(response => response.json())
+.then(responseData => {
+this.setState({
+temp: responseData.main.temp,
+desc: responseData.weather[0].description,
+icon: responseData.weather[0].icon
+});
+});
+}
+render() {
+const imgSrc = 'http://openweathermap.org/img/w/' +
+this.state.icon + '.png';
